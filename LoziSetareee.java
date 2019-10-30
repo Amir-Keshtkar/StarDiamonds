ba93adf78fcf8a0173d2dd4e08e98156e128213c
Amirhossein Keshtkar <amir78ahkl@gmail.com>
2019-10-30 13:26:12 +0330

    Add files via upload

---
+++ LoziSetareee.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class LoziSetareee {
+	public static char[][] matrix;
+	public static int N;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		N=n;
+		matrix = new char[n][2 * n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < 2 * n; j++) {
+				matrix[i][j] = ' ';
+			}
+		}
+
+		loziKon(n,n/2,n/2,-1);
+		loziKon(n,n/2,2 * n - n / 2 - 1,-1);
+		
+		loziKon(n,n/2,n/2,1);
+		loziKon(n,n/2,2 * n - n / 2 - 1,1);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < 2 * n; j++) {
+				System.out.print(matrix[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+	public static void loziKon(int n, int i,int j,int l) {
+		 if(i>=0 && i<N){
+			for(int k=0;k<=n/2;k++){
+				if(j-k>=0){
+					matrix[i][j-k]='*';
+				}
+					matrix[i][j+k]='*';
+			}
+			loziKon(n-2,i+l, j,l);
+		}
+	}
+}
