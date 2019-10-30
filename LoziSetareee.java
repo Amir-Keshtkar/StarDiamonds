b96fb6330cb6d4d4ab0e33d2a1ba64970ec3bd9f
Amirhossein Keshtkar <amir78ahkl@gmail.com>
2019-10-30 13:29:12 +0330

    Update LoziSetareee.java

--- LoziSetareee.java
+++ LoziSetareee.java
@@ -14,11 +14,11 @@ public class LoziSetareee {
 			}
 		}
 
-		loziKon(n,n/2,n/2,-1);
-		loziKon(n,n/2,2 * n - n / 2 - 1,-1);
+		MadeDiamond(n,n/2,n/2,-1);
+		MadeDiamond(n,n/2,2 * n - n / 2 - 1,-1);
 		
-		loziKon(n,n/2,n/2,1);
-		loziKon(n,n/2,2 * n - n / 2 - 1,1);
+		MadeDiamond(n,n/2,n/2,1);
+		MadeDiamond(n,n/2,2 * n - n / 2 - 1,1);
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < 2 * n; j++) {
 				System.out.print(matrix[i][j]);
@@ -27,7 +27,7 @@ public class LoziSetareee {
 		}
 	}
 
-	public static void loziKon(int n, int i,int j,int l) {
+	public static void MadeDiamond(int n, int i,int j,int l) {
 		 if(i>=0 && i<N){
 			for(int k=0;k<=n/2;k++){
 				if(j-k>=0){
@@ -35,7 +35,7 @@ public class LoziSetareee {
 				}
 					matrix[i][j+k]='*';
 			}
-			loziKon(n-2,i+l, j,l);
+			MadeDiamond(n-2,i+l, j,l);
 		}
 	}
 }
